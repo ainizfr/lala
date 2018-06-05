e84c4d763083dd2712d27840a1de05c4bc08228b
zfr； <zhangrui83@outlook.com>
2018-06-05 10:45:27 +0800

    spring

--- main/java/com/zfr/course/c06/dao/UserDAO.java
+++ main/java/com/zfr/course/c06/dao/UserDAO.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 
 import java.io.InputStream;
+import java.util.List;
 
 public class UserDAO {
     public User findUserById(int id) throws Exception {
@@ -19,5 +20,34 @@ public class UserDAO {
         sqlSession.close();
         return user;
     }
+    //根据用户名称来模糊查询用户信息列表
+    public List<User> findUserByName(String s) throws Exception{
+        String resource ="mybatis-config.xml";
+        InputStream inputStream =Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession=sqlSessionFactory.openSession();
+        List<User> users=sqlSession.selectList("com.zfr.course.c06.mapper.UserMapper.findUserByName",s);
+        sqlSession.close();
+        return users;
+    }
+    public void addUser(User user) throws Exception{
+        String resource ="mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession= sqlSessionFactory.openSession();
+        //执行SqlSession的插入方法，返回的是SQL语句影响的行数
+        int rows=sqlSession.insert("com.zfr.course.c06.mapper.UserMapper.addUser");
+        //通过返回结果判断插入操作是否执行成功
+        if(rows>0){
+            System.out.println("您成功插入了"+rows+"条数据");
+        }else{
+            System.out.println("执行插入操作失败！！！");
+        }
+        //提交事务
+        sqlSession.commit();
+        //关闭SqlSession
+        sqlSession.close();
+    }
 
 }
+
