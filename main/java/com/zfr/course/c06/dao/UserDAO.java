d481da81b98bd1b0dde408e474ea81686f007183
zfr； <zhangrui83@outlook.com>
2018-06-05 15:44:44 +0800

    spring

--- main/java/com/zfr/course/c06/dao/UserDAO.java
+++ main/java/com/zfr/course/c06/dao/UserDAO.java
@@ -7,36 +7,41 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class UserDAO {
-    public User findUserById(int id) throws Exception {
-        String resource ="mybatis-config.xml";
-        InputStream inputStream=Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession =sqlSessionFactory.openSession();
-        User user =sqlSession.selectOne("com.zfr.course.c06.mapper.UserMapper.findUserById",id);
-        sqlSession.close();
-        return user;
-    }
+     public User findUserById(int id) throws Exception {
+         String resource ="mybatis-config.xml";
+         InputStream inputStream=Resources.getResourceAsStream(resource);
+         SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(inputStream);
+         SqlSession sqlSession =sqlSessionFactory.openSession();
+         User user =sqlSession.selectOne("com.zfr.course.c06.mapper.UserMapper.findUserById",id);
+         sqlSession.close();
+         return user;
+     }
+
+
     //根据用户名称来模糊查询用户信息列表
     public List<User> findUserByName(String s) throws Exception{
         String resource ="mybatis-config.xml";
         InputStream inputStream =Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession=sqlSessionFactory.openSession();
-        List<User> users=sqlSession.selectList("com.zfr.course.c06.mapper.UserMapper.findUserByName",s);
+        List<User> users=sqlSession.selectList("com.zfr.course.c06.mapper.UserMapper.findUserByname",s);
         sqlSession.close();
         return users;
     }
+
+
     public void addUser(User user) throws Exception{
         String resource ="mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession= sqlSessionFactory.openSession();
         //执行SqlSession的插入方法，返回的是SQL语句影响的行数
-        int rows=sqlSession.insert("com.zfr.course.c06.mapper.UserMapper.addUser");
+        int rows=sqlSession.insert("com.zfr.course.c06.mapper.UserMapper.addUser",user);
         //通过返回结果判断插入操作是否执行成功
         if(rows>0){
             System.out.println("您成功插入了"+rows+"条数据");
@@ -49,5 +54,44 @@ public class UserDAO {
         sqlSession.close();
     }
 
+
+    //更新用户
+    public  void updataUser(User user) throws IOException {
+        String resource ="mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession= sqlSessionFactory.openSession();
+        int rows=sqlSession.delete("com.zfr.course.c06.mapper.UserMapper.updateUser",user);
+        if(rows>0){
+            System.out.println("您成功修改了"+rows+"条数据");
+        }else{
+            System.out.println("执行修改操作失败！！！");
+        }
+        //提交事务
+        sqlSession.commit();
+        //关闭SqlSession
+        sqlSession.close();
+    }
+
+
+    //删除用户
+    public void deleteUser(int id) throws IOException {
+        String resource ="mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession= sqlSessionFactory.openSession();
+
+        int rows=sqlSession.delete("com.zfr.course.c06.mapper.UserMapper.deleteUser",id);
+        //通过返回结果判断插入操作是否执行成功
+        if(rows>0){
+            System.out.println("您成功删除了"+rows+"条数据");
+        }else{
+            System.out.println("执行删除称、操作失败！！！");
+        }
+        //提交事务
+        sqlSession.commit();
+        //关闭SqlSession
+        sqlSession.close();
+    }
 }
 
--- test/java/test/com/zfr/course/c06/MyBatisTest.java
+++ test/java/test/com/zfr/course/c06/MyBatisTest.java
@@ -5,15 +5,42 @@ package test.com.zfr.course.c06;
         import org.springframework.context.ApplicationContext;
         import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+        import java.util.List;
+
 public class MyBatisTest {
     public static void main(String[] args){
 
         try {
             ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config-mybatis.xml");
-            UserDAO userDAO = (UserDAO) ctx.getBean("userDAO");
+            /*UserDAO userDAO = (UserDAO) ctx.getBean("userDAO");
             User user = null;
             user = userDAO.findUserById(3);
             System.out.println(user);
+            List<User> users= userDAO.findUserByName("张");
+
+
+            for(User u:users){
+                System.out.println(u);
+            }*/
+            UserDAO userDAO1 = (UserDAO) ctx.getBean("userDAO");
+            User user1 = (User) ctx.getBean("user");
+
+
+            user1.setName("张锋瑞");
+            user1.setSex("00");
+            user1.setEmail("zhangrui83@OUTLOOK.COM");
+
+            userDAO1.addUser(user1);
+            UserDAO userDAO2 = (UserDAO) ctx.getBean("userDAO");
+            User user2 = (User) ctx.getBean("user");
+            user2.setId(4);
+            user2.setName("哈哈哈");
+            user2.setSex("01");
+            user2.setEmail("734826810@qq.com");
+            userDAO2.updataUser(user2);
+            UserDAO userDAO3 = (UserDAO) ctx.getBean("userDAO");
+            userDAO3.deleteUser(1);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
